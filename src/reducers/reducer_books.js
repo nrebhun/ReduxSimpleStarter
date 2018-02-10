4529cf6fe36690de508ee52beee57338654c16b5
nrebhun <nick@nrebhun.com>
2018-02-09 19:00:18 -0800

    Complete BookDetails example

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Javascript: The Good Parts', pages: 101 },
+        { title: 'Harry Potter', pages: 679 },
+        { title: 'Foobar: Baz', pages: 70 },
+        { title: 'Cheese', pages: 1 }
+    ];
+}
